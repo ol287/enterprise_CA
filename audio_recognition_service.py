26b1b46f27faa29160d871d4c1ee3321641a88e0
olivia leese <116760201+ol287@users.noreply.github.com>
2025-02-11 14:40:31 +0000

    spelling errors

--- audio_recognition_service.py
+++ audio_recognition_service.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-AUDD_API_KEY = os.environ["AUDD_API_KEY"]
+AUDD_API_KEY = os.environ["AUDIO_API_KEY"]
 
 @app.route("/recognize", methods=["POST"])
 def recognize_audio():
